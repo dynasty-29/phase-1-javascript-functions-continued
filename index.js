9565b1aebceb8c621bd043ba2ea3f62839ee6ef3
SonnieCodes <mgathoni.gathoni9@gmail.com>
2024-11-26 05:28:34 +0300

    more functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(todo = 'roller-skate'){
+  return `This Saturday, I want to ${todo}!`;
+}
+
+function mondayWork(blues = 'go to the office'){
+  return  `This Monday, I will ${blues}.`;
+}
+
+function wrapAdjective(a = '*'){
+  return function innerFun(adj = 'special'){
+    return  `You are ${a}${adj}${a}!`;
+  }
+
+}
